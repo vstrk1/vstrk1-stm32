282337d6129fb1d1019e745309f944a6e8dcd5a7
Vladyslav <vladisen1244@gmail.com>
2026-05-18 19:10:07 +0300

    Update led by registers.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,23 @@
 
 const uint8_t LED_PIN = PC13;
 
-
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  randomSeed(analogRead(A0));
+  RCC->APB2ENR |= RCC_APB2ENR_IOPCEN; // Макрос = бітова маска, відповідає за включення тактування GPIOC
+  // Configuration Register High — налаштує піни 8–15 (PC8…PC15).
+  // PIN13 Використовує біти 23 22 21 20
+  // Інверсійне обнулення + накладання маски через AND оператор.
+  GPIOC->CRH &= ~(0xF << 20); 
+  // 11 - Push 50 MHZ General purpose push-pull
+  GPIOC->CRH |=  (0x3 << 20);
 }
 
 void loop() {
-  GPIOA->ODR |= (1U << 3);
+  // Unsigned int clear 13th bit LOW  -> LED ON
+  GPIOC->ODR &= ~(1U << 13); 
+  // volatile каже не оптимізуй
+  for (volatile uint32_t i=0; i < 900000; i++);
+  // HIGH -> LED OFF
+  GPIOC->ODR |= (1U << 13);
+  for (volatile uint32_t i=0; i < 500000; i++);
 }
 
--- src/simpleBlinker.cpp
+++ src/simpleBlinker.cpp
@@ -3,12 +3,12 @@
 const uint8_t LED_PIN = PC13;
 
 
-void setup() {
+void setupSetup() {
   pinMode(LED_PIN, OUTPUT);
   randomSeed(analogRead(A0));
 }
 
-void loop() {
+void loopLoop() {
   digitalWrite(LED_PIN, !digitalRead(LED_PIN));
   delay(random(100, 301));
 }
